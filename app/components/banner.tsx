4155c33a9aff33beadba355a135165997c8ba45c
Sam Key <samkey726@gmail.com>
2023-08-03 23:02:07 +0100

    inital settings modal

--- app/components/banner.tsx
+++ app/components/banner.tsx
@@ -1,17 +1,27 @@
 'use client';
+import { useState } from 'react';
+import { createPortal } from 'react-dom';
 import Image from 'next/image'
 
 import '../css/banner.css'
 import settingsIcon from '../ui/settings.png'
 import helpIcon from '../ui/help.png'
+import Setting from './Settings'
 
 export default function Banner() {
+    const [showModal, setShowModal] = useState(false);
+
     return (
       <div className="Wrapper">
         <h1>COVERDLE</h1>
         <div className="right-container">
-          <a onClick={Settings}><Image src={settingsIcon} width={42} height={42} alt="Open Settings"/></a>
+          <a onClick={() => setShowModal(true)}><Image src={settingsIcon} width={42} height={42} alt="Open Settings"/></a>
           <a onClick={Help}><Image src={helpIcon} width={42} height={42} alt="Open Help"/></a>
+
+          {showModal && createPortal(
+              <Setting onClose={() => setShowModal(false)} />,
+              document.body
+          )}
         </div>
       </div>
     )
--- app/components/settings.tsx
+++ app/components/settings.tsx
@@ -1,5 +1,11 @@
-export default function Settings() {
+import '../css/modal.css'
+
+export default function ModalContent({ onClose } : {onClose:any}) {
     return (
-        <h1>Hello!</h1>
-    )
+      <div className="modal">
+        <h1>Hi</h1>
+        <button onClick={onClose}>Close</button>
+      </div>
+    );
 }
+  
